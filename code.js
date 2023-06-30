2b75c740568c6480787891dcd837e11ae48870a7
Sanchit Kumar <sanchit0160@gmail.com>
2023-06-30 19:16:06 +0530

    Optimized

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
+const axios = require('axios');
 
 const YOUR_BOT_TOKEN = process.env.YOUR_BOT_TOKEN;
 const XRapidAPIKey = process.env.YOUR_XRapidAPIKey;
@@ -7,68 +8,62 @@ const XRapidAPIKey = process.env.YOUR_XRapidAPIKey;
 const bot = new TelegramBot(YOUR_BOT_TOKEN, { polling: true });
 
 const keyboard = [
-  [
-	{
-	  text: 'Get current weather',
-	  callback_data: 'current'
-	}
-  ]
+    [
+        {
+            text: 'Get current weather',
+            callback_data: 'current'
+        }
+    ]
 ];
 
 const keyboardMarkup = {
-  inline_keyboard: keyboard
+    inline_keyboard: keyboard
 };
 
-bot.on('callback_query', (callbackQuery) => {
-  const message = callbackQuery.message;
-  const chatId = message.chat.id;
+bot.on('callback_query', async (callbackQuery) => {
+    const message = callbackQuery.message;
+    const chatId = message.chat.id;
 
-  if (callbackQuery.data === 'current') {
-	getCurrentWeather(chatId);
-  }
+    if (callbackQuery.data === 'current') {
+        await getCurrentWeather(chatId);
+    }
 });
 
 bot.onText(/\/start/, (message) => {
-  const chatId = message.chat.id;
+    const chatId = message.chat.id;
 
-  bot.sendMessage(chatId, 'Welcome! Click on the button below to get the current weather:', {
-	reply_markup: keyboardMarkup
-  });
+    bot.sendMessage(chatId, 'Welcome! Click on the button below to get the current weather:', {
+        reply_markup: keyboardMarkup
+    });
 });
 
-function getCurrentWeather(chatId) {
-  fetch('http://ip-api.com/json')
-    .then(response => response.json())
-    .then(data => {
-      const latitude = data.lat.toFixed(2);
-      const longitude = data.lon.toFixed(2);
+async function getCurrentWeather(chatId) {
+    try {
+        const ipApiResponse = await axios.get('http://ip-api.com/json');
+        const { lat, lon } = ipApiResponse.data;
+        const latitude = lat.toFixed(2);
+        const longitude = lon.toFixed(2);
 
-      const query = `${latitude},${longitude}`;
-      const options = {
-        method: 'GET',
-        headers: {
-          'X-RapidAPI-Key': XRapidAPIKey,
-          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-        }
-      };
-      
-      fetch(`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`, options)
-        .then(response => response.json())
-        .then(data => {
-          const animationUrl = 'https://media.giphy.com/media/za5xikuRr0OzK/giphy.gif';
-          bot.sendAnimation(chatId, animationUrl)
-            .then(() => {
-              const messageText = `The weather in ${data.location.name} is ${data.current.condition.text} with a temperature of ${data.current.temp_c} °C.`;
-              bot.sendMessage(chatId, messageText)
-              	.then(() => {              	  
-              	  const buttonMessage = 'Get the current weather again.';
-              	  bot.sendMessage(chatId, buttonMessage, { reply_markup: keyboardMarkup });
-              	})
-              	.catch(error => console.error(error));
-            })
-            .catch(error => console.error(error));
-        })
-        .catch(error => console.error(error));
-    })
-    .catch(error => console.error(error));
+        const query = `${latitude},${longitude}`;
+        const options = {
+            headers: {
+                'X-RapidAPI-Key': XRapidAPIKey,
+                'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+            }
+        };
+
+        const weatherApiResponse = await axios.get(`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`, options);
+        const { location, current } = weatherApiResponse.data;
+
+        const animationUrl = 'https://media.giphy.com/media/za5xikuRr0OzK/giphy.gif';
+        await bot.sendAnimation(chatId, animationUrl);
+
+        const messageText = `The weather in ${location.name} is ${current.condition.text} with a temperature of ${current.temp_c} °C.`;
+        await bot.sendMessage(chatId, messageText);
+
+        const buttonMessage = 'Get the current weather again.';
+        await bot.sendMessage(chatId, buttonMessage, { reply_markup: keyboardMarkup });
+    } catch (error) {
+        console.error(error);
+    }
 }
